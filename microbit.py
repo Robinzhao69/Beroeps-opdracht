b7c1b4827b7694ffcaf37272376e8529cd1d2ae1
Robin <titanen.197@hotmail.com>
2020-09-17 23:05:21 +0200

    compass

---
+++ microbit.py
@@ -0,0 +1,17 @@
+from microbit import *
+
+compass.calibrate()
+
+while True:
+    if button_a.was_pressed():
+        display.scroll(str(compass.heading()))
+        
+    if compass.heading() < 45 or compass.heading() > 315:
+        display.show("N")
+    elif compass.heading() < 135:
+        display.show("E")
+    elif compass.heading() < 225:
+        display.show("S")
+    else:
+        display.show("W")
+    
