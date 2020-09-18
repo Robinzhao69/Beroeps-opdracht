1c805db388f29a00dd168f9cd1d932d65fe41f93
Robin <titanen.197@hotmail.com>
2020-09-18 15:07:43 +0200

    hele mooie vragen

---
+++ ditbenik.py
@@ -0,0 +1,36 @@
+print("Hallo ik ben Robin")
+
+naam = input("Wie ben jij?")
+
+print("Hoi " + naam)
+
+print("Ik ben een nieuwkomer op het Mediacollege amsterdam. ik ga je wat vragen stellen over mezelf, zodat jij uiteindelijk meer over mij te weten komt.")
+
+antwoord1 = input("Waar zat ik hier voor op school? \nA. Tabor college D'ampte \nB. E-commerce school van Jia \nC. Geen school \nantwoord: ")
+if antwoord1 == "a" or antwoord1 == "A":
+    print("jup ik zat op het Tabor college hier voor.")
+    print("\n")
+else:
+    print("fout het antwoord was A.")
+    print("\n")
+
+antwoord2 = input("Wat zijn mijn Hobby's? \nA. slapen en eten \nB. simpen en keyboard warrior zijn \nC. gamen en voetballen \nantwoord: ")
+if antwoord2 == "c" or antwoord2 == "C":
+    print("yes ik vind deze dingen zeker leuk.")
+    print("\n")
+else:
+    print("nope helaas niet")
+    print("\n")
+
+antwoord3 = input("Wat deden we op de eerst dag op school? \nA. game maken \nB. aan microbit werken \nC. helemaal niks \nantwoord: ")
+if antwoord3 == "b" or antwoord3 == "B":
+    print("jij bent echt super goed en slim")
+    print("\n")
+else:
+    print("was het maar zo'n feest")
+    print("\n")
+
+print("Oke dit was het einde van mijn mooie quiz.") 
+
+
+
