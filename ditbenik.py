ef6afd83f4ca628ae8c4fb4fec35063852595778
Robin <titanen.197@hotmail.com>
2020-09-27 20:02:45 +0200

    geupdate

--- ditbenik.py
+++ ditbenik.py
@@ -1,3 +1,5 @@
+
+
 print("Hallo ik ben Robin")
 
 naam = input("Wie ben jij?")
@@ -6,31 +8,177 @@ print("Hoi " + naam)
 
 print("Ik ben een nieuwkomer op het Mediacollege amsterdam. ik ga je wat vragen stellen over mezelf, zodat jij uiteindelijk meer over mij te weten komt.")
 
+print("-------------------------------------------------------------------------------------------------------------------------")
 antwoord1 = input("Waar zat ik hier voor op school? \nA. Tabor college D'ampte \nB. E-commerce school van Jia \nC. Geen school \nantwoord: ")
 if antwoord1 == "a" or antwoord1 == "A":
+    print("\n")
     print("jup ik zat op het Tabor college hier voor.")
     print("\n")
 else:
+    print("\n")
     print("fout het antwoord was A.")
     print("\n")
 
+print("-------------------------------------------------------------------------------------------------------------------------")
 antwoord2 = input("Wat zijn mijn Hobby's? \nA. slapen en eten \nB. simpen en keyboard warrior zijn \nC. gamen en voetballen \nantwoord: ")
 if antwoord2 == "c" or antwoord2 == "C":
+    print("\n")
     print("yes ik vind deze dingen zeker leuk.")
     print("\n")
 else:
+    print("\n")
     print("nope helaas niet")
     print("\n")
 
+print("-------------------------------------------------------------------------------------------------------------------------")
 antwoord3 = input("Wat deden we op de eerst dag op school? \nA. game maken \nB. aan microbit werken \nC. helemaal niks \nantwoord: ")
 if antwoord3 == "b" or antwoord3 == "B":
+    print("\n")
     print("jij bent echt super goed en slim")
     print("\n")
 else:
+    print("\n")
     print("was het maar zo'n feest")
     print("\n")
+ 
+print("-------------------------------------------------------------------------------------------------------------------------")
+antwoord4 = input("Wil je nog meer vragen beantwoorden? \nJa \nNee \nantwoord: ")
+if antwoord4 == "Ja" or antwoord4 == "ja":
+    print("\n")
+    print("Ok laten we lekker makkelijk door gaan")
+    print("\n")
 
-print("Oke dit was het einde van mijn mooie quiz.") 
+    print("-------------------------------------------------------------------------------------------------------------------------")
+    antwoord5 = input("Welke positie sta ik bij voetbal \nA. spits \nB. keeper \nC. bank \nantwoord: ")
+    if antwoord5 == "a" or antwoord5 == "A":
+        print("\n")
+        print("lekker bezig")
+        print("\n")
+    else: 
+        ohoh = input("Heb jij wel zitten opletten? \nJa \nNee \nantwoord: ")
+        if ohoh == "ja" or ohoh == "Ja":
+            print("\n")
+            print("Ok laten we dan maar door gaan")
+            print("\n")
 
+            print("-------------------------------------------------------------------------------------------------------------------------")                        
+            antwoord6 = input("Hoelang game ik per dag? \nA. 24 uur \nB. 20 min \nC. zeg ik lekker niet \nantwoord: ")
+            if antwoord6 == "c" or antwoord6 == "C":
+                print("\n")
+                print("Correct. Het is beter als je het niet weet.")
+                print("\n")
+            else:
+                print("\n")
+                print("Ben je gek geen van beide zijn goed")
+                print("\n")
 
+                print("-------------------------------------------------------------------------------------------------------------------------")            
+                antwoord7 = input("En als laatste vraag. In welk jaar ben ik geboren. \nA. 1987 \nB. 2420 \nC. 2004 \nantwoord: ")
+                if antwoord7 == "c" or antwoord7 == "C":
+                    print("\n")
+                    print("helemaal goed")
+                    print("\n")
+                else:
+                    print("\n")
+                    print("Beter opletten de volgende keer")
+                    print("\n")
+                    quit
+        else:
+            print("\n")
+            print("Ga maar opnieuw de quiz terug lezen")
+            quit
+    
+    print("-------------------------------------------------------------------------------------------------------------------------")    
+    antwoord6 = input("Hoelang game ik per dag? \nA. 24 uur \nB. 20 min \nC. zeg ik lekker niet \nantwoord: ")
+    if antwoord6 == "c" or antwoord6 == "C":
+        print("\n")
+        print("Correct. Het is beter als je het niet weet.")
+        print("\n")
+    else:
+        print("\n")
+        print("Ben je gek geen van beide zijn goed")
+        print("\n") 
 
+    print("-------------------------------------------------------------------------------------------------------------------------")            
+    antwoord7 = input("En als laatste vraag. In welk jaar ben ik geboren. \nA. 1987 \nB. 2420 \nC. 2004 \nantwoord: ")
+    if antwoord7 == "c" or antwoord7 == "C":
+        print("\n")
+        print("helemaal goed")
+        print("\n")
+    else:
+        print("\n")
+        print("Beter opletten de volgende keer")
+        print("\n")
+        quit  
+
+else:
+    print("jammer dan. speciaal voor jou gaan we het moeilijker maken.")
+    print("\n")
+
+    print("-------------------------------------------------------------------------------------------------------------------------")
+    subantwoord1 = input("Wat is mijn favoriete game om samen te spelen met vrienden? \nA. krunker.io \nB. among us \nC. dungeon and dragons \nantwoord: ")
+    if subantwoord1 == "b" or subantwoord1 == "B":
+        print("\n")
+        print("ja het is een grappig spel je moet het echt uit proberen")
+        print("\n")
+    else:
+        print("-------------------------------------------------------------------------------------------------------------------------")
+        ohoh2 = input("Heb jij wel zitten opletten? \nJa \nNee \nantwoord: ")
+        if ohoh2 == "ja" or ohoh2 == "Ja":
+            print("\n")
+            print("Ok laten we dan maar door gaan")
+            print("\n")
+        else:
+            print("\n")
+            print("Ga maar opnieuw de quiz terug lezen")
+            print("\n")
+            
+            
+            print("-------------------------------------------------------------------------------------------------------------------------")
+            antwoord6 = input("Hoelang game ik per dag? \nA. 24 uur \nB. 20 min \nC. zeg ik lekker niet \nantwoord: ")
+            if antwoord6 == "c" or antwoord6 == "C":
+                print("\n")
+                print("Correct. Het is beter als je het niet weet.")
+                print("\n")
+            else:
+                print("\n")
+                print("Ben je gek geen van beide zijn goed")
+                print("\n") 
+
+                print("-------------------------------------------------------------------------------------------------------------------------")
+                antwoord7 = input("En als laatste vraag. In welk jaar ben ik geboren. \nA. 1987 \nB. 2420 \nC. 2004 \nantwoord: ")
+                if antwoord7 == "c" or antwoord7 == "C":
+                    print("\n")
+                    print("helemaal goed")
+                    print("\n")
+                else:
+                    print("\n")
+                    print("Beter opletten de volgende keer")
+                    print("\n")
+                    quit
+    
+    print("-------------------------------------------------------------------------------------------------------------------------")
+    antwoord6 = input("Hoelang game ik per dag? \nA. 24 uur \nB. 20 min \nC. zeg ik lekker niet \nantwoord: ")
+    if antwoord6 == "c" or antwoord6 == "C":
+            print("\n")
+            print("Correct. Het is beter als je het niet weet.")
+            print("\n")
+    else:
+        print("\n")
+        print("Ben je gek geen van beide zijn goed")
+        print("\n") 
+
+    print("-------------------------------------------------------------------------------------------------------------------------")
+    antwoord7 = input("En als laatste vraag. In welk jaar ben ik geboren. \nA. 1987 \nB. 2420 \nC. 2004 \nantwoord: ")
+    if antwoord7 == "c" or antwoord7 == "C":
+            print("\n")
+            print("helemaal goed")
+            print("\n")
+    else:
+        print("\n")
+        print("Beter opletten de volgende keer")
+        print("\n")
+        quit        
+        
+        
+        print("Dit was het einde van mijn quiz.")       
